85d68ba730f7490b146f035cb7d86f5dac4959e1
Mara Jeh <168854597+mara-jeh@users.noreply.github.com>
2024-11-01 14:00:07 -0300

    primeira versão

---
+++ perguntas.js
@@ -0,0 +1,30 @@
+criaCartao(
+    'Biologia',
+    'O que é um Mamífero?',
+    'são animais que possuem pelos e glândulas mamárias'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Japão?',
+    'A capital da Japão é Tokio'
+)
+
+criaCartao(
+    'Filosofia',
+    'De quem é a famosa frase “Penso, logo existo”?',
+    'a frase original, escrita em francês, do filósofo René Descartes (1596-1650)'
+)
+
+criaCartao(
+    'Tecnologia',
+    'De onde é a invenção do chuveiro elétrico?',
+    'Brasil'
+)
+
+
+criaCartao(
+    'Matemática',
+    'Quantas casas decimais tem o número pi?',
+    'Infinitas'
+)
